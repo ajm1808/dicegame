9c19f46c9de7bcba9afc7e152fd93270481cd1d1
ajm1808 <ajm180885@gmail.com>
2020-05-11 06:58:17 +0100

    completed 2dice game

--- Dicegame2/script.js
+++ Dicegame2/script.js
@@ -1,6 +1,11 @@
 let scores, roundScore, activePlayer, gamePlaying;
 
-const bNew=document.querySelector("btn-new");
+const bNew=document.querySelector(".btn-new");
+const bRoll=document.querySelector(".btn-roll");
+const bHold=document.querySelector(".btn-hold");
+
+const dice1=document.getElementById("dice1");
+const dice2=document.getElementById("dice2");
 
 function start() {
     scores= [0,0];
@@ -8,8 +13,8 @@ function start() {
     roundScore=0;
     gamePlaying=true;
 
-    document.getElementById("dice1").style.display="none";
-    document.getElementById("dice2").style.display="none";
+    dice1.style.display="none";
+    dice2.style.display="none";
 
     document.getElementById("score-0").textContent="0";
     document.getElementById("score-1").textContent="0";
@@ -19,20 +24,88 @@ function start() {
     document.getElementById("name-0").textContent="Player 1";
     document.getElementById("name-1").textContent="Player 2";
     
-    document.querySelector('.player-0-panel').classList.Remove('winner');
+    document.querySelector(".player-0-panel").classList.remove('winner');
     document.querySelector(".player-1-panel").classList.remove('winner');
     document.querySelector(".player-0-panel").classList.remove('active');
     document.querySelector(".player-1-panel").classList.remove('active');
     document.querySelector(".player-0-panel").classList.add('active');   
 }
 start();
-bNew.addEventListener
 
-document.querySelector("btn-roll").addEventListener("click",()=>{
+// bNew.addEventListener("click",start);
+
+bRoll.addEventListener('click',()=>{
    if(gamePlaying){
-       let d1=Math.floor(Math.random()*6)+1;
-       let d2=Math.floor(Math.random()*6)+1;
-    console.log(d1+""+d2);
-   }
+    //if game is playing the button roll will cause the function to generate 2 random numbers   
+    let d1=Math.floor(Math.random()*6)+1;
+    let d2=Math.floor(Math.random()*6)+1;
+
+    //dice will only appear (block) when roll button is clicked. The numbers that appear will match the numbers of d1 & d2
+    dice1.style.display="block";
+    dice2.style.display="block";
+    dice1.src="img/dice"+d1+".png";
+    dice2.src="img/dice"+d2+".png";
+
+    //skipping to player 2 if dice is equal to 1 and added score to total
+    if(d1 !==1&& d2!==1){
+        roundScore+=+d1+d2;
+        document.querySelector("#current-"+activePlayer).textContent=roundScore;
+    }
+    else {
+        nextPlayer();
+    }
+    }
     
-})
+});
+
+function nextPlayer(){
+    if(activePlayer===0){
+        activePlayer=1;
+    }
+    else{
+        activePlayer=0;
+    }
+    roundScore=0;
+    document.getElementById("current-0").textContent="0";
+    document.getElementById("current-1").textContent="0";
+
+    document.querySelector(".player-0-panel").classList.toggle("active");
+    document.querySelector(".player-1-panel").classList.toggle("active");
+
+    dice1.style.display="none";
+    dice2.style.display="none";
+};
+
+bHold.addEventListener("click",()=>{
+    if(gamePlaying){
+        scores[activePlayer]+=roundScore;
+
+        document.querySelector("#score-"+activePlayer).textContent=scores[activePlayer];
+
+        let input=document.querySelector(".final-score").value;
+        let winningScore;
+
+        if(input){
+            winningScore=input;
+        }
+        else{
+            winningScore=40;
+        }
+
+        //check if player has won the game
+        if(scores[activePlayer]>=winningScore){
+
+            document.querySelector("#name-"+activePlayer).textContent="Winner!";
+            dice1.style.display="none";
+            dice2.style.display="none";
+
+            document.querySelector(".player-"+activePlayer+"-panel").classList.add("winner");
+            document.querySelector(".player-"+activePlayer+"-panel").classList.remove("active");
+
+            gamePlaying=false;
+        }
+        else {
+            nextPlayer();
+        }
+    }
+});
